59124f752ce9dc27a089ea2e576c6849c277a331
Ivan Klimchuk <alroniks@gmail.com>
2024-03-23 16:05:20 +0300

    Add more commands

--- functions/bot.js
+++ functions/bot.js
@@ -1,15 +1,27 @@
-const { Telegraf } = require("telegraf")
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const { Telegraf } = require("telegraf");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start(ctx => {
-  console.log("Received /start command")
-  try {
-    return ctx.reply("Hi")
-  } catch (e) {
-    console.error("error in start action:", e)
-    return ctx.reply("Error occurred")
-  }
-})
+const messages = {
+  "donate": "Каманда, якая паказвае тэкст пра данаты",
+  "knigi": "Каманда, што піша пра кнігі",
+  "suviaz": "Каманда для сувязі",
+};
+
+Object.keys(messages).forEach(function(command) {
+  console.log(`Received '${command}'' command`);
+  bot.command(command, async (ctx) => await ctx.reply(messages[command]));
+});
+
+
+// bot.start(ctx => {
+//   console.log("Received /start command")
+//   try {
+//     return ctx.reply("Вітаю! Гэта бот і ён прызначаны, каб вітацца!")
+//   } catch (e) {
+//     console.error("error in start action:", e)
+//     return ctx.reply("Error occurred")
+//   }
+// })
 
 exports.handler = async event => {
   try {
